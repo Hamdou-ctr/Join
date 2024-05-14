3b3ab42fcfac3f12b52e21c1c62746f8414a7afa
Hamdou-ctr <hamidou-diallo@gmx.de>
2024-05-14 19:52:48 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+async function init() {
+  await includeHTML();
+  hover();
+}
+
+async function includeHTML() {
+  let includeElements = document.querySelectorAll("[w3-include-html]");
+  for (let i = 0; i < includeElements.length; i++) {
+    const element = includeElements[i];
+    file = element.getAttribute("w3-include-html"); // "includes/header.html"
+    let resp = await fetch(file);
+    if (resp.ok) {
+      element.innerHTML = await resp.text();
+    } else {
+      element.innerHTML = "Page not found";
+    }
+  }
+}
+
+function hover() {
+  const hoverDiv = document.getElementById("hoverDiv");
+
+  hoverDiv.addEventListener("mouseover", function () {
+    hoverDiv.classList.add("hovered");
+  });
+
+  hoverDiv.addEventListener("mouseout", function () {
+    hoverDiv.classList.remove("hovered");
+  });
+}
